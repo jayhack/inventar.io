3ccf6276807ff7f17f9f88b8a99a35eb1018319b
Jay Hack <jhack@stanford.edu>
2015-05-12 17:40:34 -0700

    added tengo webhook

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ Contains main flask application
 from flask import Flask
 from inventario import Inventario
 
+#=====[ Step 1: setup	]=====
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +17,10 @@ def index():
 @app.route('/quiero')
 def quiero():
 	return "... quiero ..."
+
+@app.route('/tengo')
+def tengo():
+	return "... tengo ..."
 
 if __name__ == '__main__':
 	app.run()
