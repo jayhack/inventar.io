4b32d451c044da7e6b67c81e37f946c770c70456
Jay Hack <jhack@stanford.edu>
2015-05-13 17:19:06 -0700

    trying again

--- setup_db.py
+++ setup_db.py
@@ -3,7 +3,7 @@ import sqlite3
 if __name__ == '__main__':
 
 	#=====[ Step 1: connect	]=====
-	conn = sqlite3.connect('/home/jayhack/inventar.io')
+	conn = sqlite3.connect('/home/jayhack/inventar.db')
 	c = conn.cursor()
 
 	#=====[ Step 2: clear	]=====
