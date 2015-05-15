cbe158d1a7f9c541884871bbf8539ffd513be5a4
Jay Hack <jhack@stanford.edu>
2015-05-14 18:32:18 -0700

    cosmetic change

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def index():
 @app.route('/wiki', methods=['POST'])
 def wiki():
 	"""returns wikipedia article"""
-	print "GOT A WIKIPEDIA REQUEST (not supported yet.)"
+	print "=====[ GOT A WIKIPEDIA REQUEST ]====="
 	return ''
 
 @app.route('/quiero', methods=['POST'])
