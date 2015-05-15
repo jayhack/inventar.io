d459b804d1c38635d56dfa472190f88ff14919bb
Jay Hack <jhack@stanford.edu>
2015-05-15 00:13:53 -0700

    added 'list' function to DBClient

--- dbclient.py
+++ dbclient.py
@@ -20,10 +20,18 @@ class DBClient(object):
 		for item in items:
 			self.orc_client.put(collection, str(uuid.uuid4()), item)
 
+	def page_to_item(self, page):
+		"""converts NoSQL pages to the items that were inserted"""
+		return page['value']
+
 	def search(self, collection, key, value):
 		"""searches 'collection' for items with specified key/value pair"""
 		search = porc.Search().query('value.%s:%s' % (key, value))
 		pages = self.orc_client.search(collection, search)
-		return [p['value'] for p in pages.all()]
+		return [self.page_to_item(p) for p in pages.all()]
 
+	def list(self, collection):
+		"""returns all items in the collection"""
+		pages = self.orc_client.list(collection)
+		return [self.page_to_item(p) for p in pages.all()]
 
