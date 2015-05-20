0da78fdf71d21a05d5906f99e0c6565865efc9cf
Jay Hack <jhack@stanford.edu>
2015-05-19 19:30:02 -0700

    not serving static well right now...

--- landing_page.py
+++ landing_page.py
@@ -8,7 +8,7 @@ import webapp2
 import jinja2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), './static')),
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'static')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
