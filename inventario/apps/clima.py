f2052c0252f04bfe9b9d59ad1d456ead4f53ffd4
Jay Hack <jhack@stanford.edu>
2015-05-21 03:18:47 -0700

    .

--- inventario/apps/clima.py
+++ inventario/apps/clima.py
@@ -1,5 +1,6 @@
 from ..app_base import EmailAppBase
 import forecastio
+import pygeocoder
 
 class App(EmailAppBase):
 	"""
@@ -8,18 +9,33 @@ class App(EmailAppBase):
 	Returns weather forecast for next 3 hours
 	"""
 
+
 	#=====[ Metadata	]=====
-	dependencies = ['forecastio']
+	dependencies = ['forecastio', 'pygeocoder']
 	API_KEY = '1f5afb17bcb9c1b1a63b2349866a89a8'
 	from_email = 'clima@ivioapp.com'
 	header = 'Prognostico del Tiempo%s======================'
+	havana_gps = (23.1333, 82.3833)
 
 
-	def make_response(self, forecast):
+	def get_gps(self, mail):
+		"""mail -> (lat, long) gps coords to be used"""
+		if len(mail.subject().strip()) > 3:
+			try:
+				result = pygeocoder.Geocoder.geocode(mail.subject())
+				return result[0].coordinates, mail.subject().strip()
+			except:
+				return self.havana_gps, 'La Habana'
+		else:
+			return self.havana_gps, 'La Habana'
+
+
+	def make_response(self, location, forecast):
 		"""forecastio forecast -> response"""
 		return """
 Prognostico del Tiempo
 ======================
+Tiempo en %s
 
 Current:
 --------
@@ -32,21 +48,21 @@ Hourly:
 Daily:
 ------
 %s
-""" 	% (	
+""" 	% (	location,
 			forecast.currently().summary, 
 			forecast.hourly().summary, 
 			forecast.daily().summary
 			)
 
-
-
-
 	def process(self, mail):
+		#=====[ Step 1: get lat/long	]=====
+		gps, location = self.get_gps(mail)
+
 		#=====[ Step 1: get forecast	]=====
-		forecast = forecastio.load_forecast(self.API_KEY, 23.1333, 82.3833)
+		forecast = forecastio.load_forecast(self.API_KEY, gps[0], gps[1])
 
 		#=====[ Step 2: send result	]=====
-		body = self.make_response(forecast)
+		body = self.make_response(location, forecast)
 		self.email_client.send_message(	
 										self.from_email,
 										mail.user, 
