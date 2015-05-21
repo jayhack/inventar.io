635a4eaeafa5c9196a2507053a592f4256380725
Jay Hack <jhack@stanford.edu>
2015-05-21 03:10:47 -0700

    modified clima

--- inventario/apps/clima.py
+++ inventario/apps/clima.py
@@ -5,22 +5,51 @@ class App(EmailAppBase):
 	"""
 	App: Clima
 	==========
-	Returns current weather in Havana
+	Returns weather forecast for next 3 hours
 	"""
 
 	#=====[ Metadata	]=====
 	dependencies = ['forecastio']
 	API_KEY = '1f5afb17bcb9c1b1a63b2349866a89a8'
 	from_email = 'clima@ivioapp.com'
+	header = 'Prognostico del Tiempo%s======================'
+
+
+	def make_response(self, forecast):
+		"""forecastio forecast -> response"""
+		return """
+Prognostico del Tiempo
+======================
+
+Current:
+--------
+%s
+
+Hourly:
+-------
+%s
+
+Daily:
+------
+%s
+""" 	% (	
+			forecast.currently().summary, 
+			forecast.hourly().summary, 
+			forecast.daily().summary
+			)
+
+
+
 
 	def process(self, mail):
 		#=====[ Step 1: get forecast	]=====
-		tiempo = forecastio.load_forecast(self.API_KEY, 23.1333, 82.3833)
+		forecast = forecastio.load_forecast(self.API_KEY, 23.1333, 82.3833)
 
 		#=====[ Step 2: send result	]=====
+		body = self.make_response(forecast)
 		self.email_client.send_message(	
 										self.from_email,
 										mail.user, 
-										'prognostico del tiempo', 
-										str(tiempo.hourly())
+										'Prognostico del Tiempo', 
+										body
 									)
