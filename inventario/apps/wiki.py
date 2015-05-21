672df9a7bc5178b5b0e0ef7b66343e971ef1f478
Jay Hack <jhack@stanford.edu>
2015-05-21 03:28:01 -0700

    formatting on wiki now too

--- inventario/apps/wiki.py
+++ inventario/apps/wiki.py
@@ -12,12 +12,33 @@ class App(EmailAppBase):
 	dependencies = ['wikipedia']
 	from_email = 'wiki@ivioapp.com'
 
+	def get_summary(self, email):
+		"""returns formatted summary"""
+		#=====[ Step 1: get summary	]=====
+		try:
+			summary = wikipedia.summary(mail.subject.strip())
+		except:
+			summary = 'Disambiguation Error. Try English!'
+
+		#=====[ Step 2: format	]=====
+		return """
+Wikipedia Summary:
+==================
+
+%s
+---
+
+%s
+""" % (mail.subject.strip(), summary)
+
+
+
 	def process(self, email):
 		#=====[ Step 1: get summary	]=====
 		try:
 			summary = wikipedia.summary(mail.subject.strip())
 		except:
-			summary = 'Disambiguation Error'
+			summary = 'Disambiguation Error. Try English!'
 
 		#=====[ Step 2: send results	]=====
 		self.email_client.send_message(
