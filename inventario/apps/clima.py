737670c83550714f0e2baf9d9c1d8085085fc949
Jay Hack <jhack@stanford.edu>
2015-05-21 01:38:35 -0700

    .

--- inventario/apps/clima.py
+++ inventario/apps/clima.py
@@ -11,8 +11,16 @@ class App(EmailAppBase):
 	#=====[ Metadata	]=====
 	dependencies = ['forecastio']
 	API_KEY = '1f5afb17bcb9c1b1a63b2349866a89a8'
+	from_email = 'clima@ivioapp.com'
 
 	def process(self, mail):
+		#=====[ Step 1: get forecast	]=====
 		tiempo = forecastio.load_forecast(self.API_KEY, 23.1333, 82.3833)
-		self.email_client.send_message(mail.user, 'prognostico del tiempo', 
-										str(tiempo.hourly()))
+
+		#=====[ Step 2: send result	]=====
+		self.email_client.send_message(	
+										self.from_email,
+										mail.user, 
+										'prognostico del tiempo', 
+										str(tiempo.hourly())
+										)
