422a8baf56859d2f5a308eebf8fc05a8198b4fe6
Jay Hack <jhack@stanford.edu>
2015-05-20 20:16:05 -0700

    cleaned up main

--- apps/clima.py
+++ apps/clima.py
@@ -21,7 +21,6 @@ class App(AppBase):
 	dependencies = ['forecastio']
 	API_KEY = '1f5afb17bcb9c1b1a63b2349866a89a8'
 
-
 	def process(self, mail):
 		tiempo = forecastio.load_forecast(self.API_KEY, 23.1333, 82.3833)
 		self.mail_client.send_message(mail.user, 'prognostico del tiempo', 
