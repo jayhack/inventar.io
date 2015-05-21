47b68bbcecf9f9c7f6f259f53af38d3250d63e4d
Jay Hack <jhack@stanford.edu>
2015-05-21 02:50:12 -0700

    yikyak first try

--- inventario/apps/yikyak.py
+++ inventario/apps/yikyak.py
@@ -1,82 +1,43 @@
-from ..inventario import MailAppBase
 import pprint
+from ..inventario import MailAppBase
 
 class App(MailAppBase):
 	"""
 	App: YikYak
 	===========
 
-	Allows users to post to and view an anonymous feed
+	Allows users to post to and view an anonymous feed. 
 	"""
 	#=====[ metadata ]=====
+	from_email = 'yikyak@ivioapp.com'
+	db_collection = 'yikyak'
 	dependencies = ['pprint']
 	char_limit = 140
+	feed_header = """YIKYAK FEED:\n============\n\n"""
 
-	def get_feed(self, mail):
+	def post_to_str(self, post):
+		"""post -> string"""
+		return "-----\n%s\n" % post['text']
+
+	def get_feed(self):
 		"""returns current feed as string"""
-		return pprint.pformat(self.db_client.list(self.db_collection))
+		posts = self.db_client.list(self.db_collection)
+		post_strs = [self.post_to_str(p) for p in reversed(posts)]
+		return self.feed_header + '\n'.join(post_strs)
 
-	def process_feed(self, mail):
-		"""processes yikyak reads"""
-		feed = self.get_feed(mail)
-		self.mail_client.send_message(mail.user, 'YikYak Feed', feed)
-
-	def process_post(self, mail):
+	def submit_yak(self, mail):
 		"""processes yikyak posts; limit 140 characters"""
 		post = mail.subject.strip()[:self.char_limit]
 		self.db_client.put(self.db_collection, {'text':post, 'user':mail.user})
-		feed = self.get_feed(mail)
-		self.mail_client.send_message(mail.user, 'YikYak Feed', feed)
 
 	def process(self, mail):
-		if len(mail.subject.strip()) == 0:
-			self.process_feed(mail)
-		else:
-			self.process_post(mail)
+		#=====[ Step 1: submit post	]=====
+		if len(mail.subject.strip()) > 3:
+			self.submit_yak(mail)
 
-
-"""
-Module: yikyak
-=================
-
-Contains abstract class YikYakEmail
-
-"""
-import re
-import pprint
-from datetime import datetime
-from mail import IvioMail
-
-class YikYakEmail(object):
-
-	def __init__(self, mail):
-		"""
-		Args:
-		-----
-		- mail: IvioMail object
-		"""
-
-		self.votes = self.get_vote_lines(mail.body)
-		self.post = self.extract_post(mail)
-		# address = self.extract_address(mail.body)
-		# item_lines = self.get_item_lines(mail.body)
-		# self.items = [self.get_item(l, mail.user, address, mail.date) for l in item_lines]
-
-	def extract_post(self, mail):
-		matches = re.findall(r'--.*--', mail.body)
-		if len(matches) == 0:
-			return None
-		return {
-				'post':matches[0],
-				'user':mail.user,
-				'date':mail.date,
-				'votes':0
-				}
-
-	def get_vote_lines(self, body):
-		return filter(lambda l: l.strip().startswith('@'), body.split('\n'))
-
-	def __str__(self):
-		return '=====[IvioEmail]=====\n%s' % pprint.pformat(self.items)
-
-
+		#=====[ Step 2: send results	]=====
+		from_email = self.from_email
+		to_email = mail.user
+		subject = 'yikyak feed'
+		body = self.get_feed()
+		self.email_client.send_message(from_email, to_email, subject, body)
