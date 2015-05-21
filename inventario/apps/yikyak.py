065d4ff1149a979e8cbe88f394da5a4e3fe565c2
Jay Hack <jhack@stanford.edu>
2015-05-21 03:04:06 -0700

    yikyak is now working

--- inventario/apps/yikyak.py
+++ inventario/apps/yikyak.py
@@ -18,7 +18,7 @@ class App(EmailAppBase):
 		"""post -> string"""
 		if not 'post' in post:
 			print post
-		return "-----\n%s\n" % post['post']
+		return "%s----------\n" % post['post']
 
 	def get_feed(self):
 		"""returns current feed as string"""
