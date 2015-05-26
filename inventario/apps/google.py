e3f2de41ccab44a53798fb802147a820fa114fd7
akiel <jaorozco@estudiantes.uci.cu>
2015-05-25 21:22:03 -0400

    google application added

---
+++ inventario/apps/google.py
@@ -0,0 +1,61 @@
+from ..app_base import EmailAppBase
+from google import search
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+class App(EmailAppBase):
+	"""
+	App: Google
+	=========
+	Returns first 10 google results for the subject
+	"""
+
+	#=====[ Metadata	]=====
+	dependencies = ['google','BeautifulSoup','urllib2']
+	from_email = 'google@ivioapp.com'
+
+	def get_search(self, email):
+		"""returns formatted search"""
+		#=====[ Step 1: get search results	]=====
+		try:
+			search = ""
+			c = 0
+			for url in search(email.subject.strip(),lang='es'):
+				if c == 10:
+					break
+				c+=1
+				try:
+					data = urllib2.urlopen(url).read()
+					search += BeautifulSoup(data).html.head.title.contents
+				except:
+					search += "No se puede obtener titulo para esta direccion"
+					pass
+				search += url + "\n"
+
+		except:
+			search = 'Error buscando '+email.subject.strip()
+
+		#=====[ Step 2: format	]=====
+		return """
+Resultados de Google:
+==================
+
+%s
+---
+
+%s
+""" % (email.subject.strip(), search)
+
+
+
+	def process(self, email):
+		#=====[ Step 1: get body	]=====
+		body = self.get_search(email)
+
+		#=====[ Step 2: send results	]=====
+		self.email_client.send_message(
+										self.from_email, 
+										email.user, 
+										'Google Resultados', 
+										body
+										)
